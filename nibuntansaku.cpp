c279118d2d98e0e7407512f7e55b08cd889a1716
Riku Yukimaru <setsugan97@gmail.com>
2025-09-18 15:09:15 +0900

    見た目を整えた
    カーリーブラケットの配置と、簡単なコメント、デバッグ機能の削除

--- nibuntansaku.cpp
+++ nibuntansaku.cpp
@@ -13,49 +13,48 @@ int main(void)
 
     std::sort(numbers.begin(), numbers.end());
 
-    for(auto &number : numbers)
-    {
-        cout << number << " ";
-    }
-    cout << endl;
-
-    //---
-    //二分探索
-    //---
     int target;
     cin >> target;
 
     int min = 0; 
     int max = n - 1;
 
-    int now_looking_num;
+    int now_looking_num; // 探索範囲の中央の値
 
     bool isSearching = true;
 
     while(isSearching){
         now_looking_num = numbers[(max + min) / 2];
 
-        if(max == min){
-            if(numbers[max] == target){
+        if(max == min)
+        {
+            if(numbers[max] == target)
+            {
                 cout << "I found the number." << endl;
             }
-            else{
+            else
+            {
                 cout << "not found" << endl;
             }
             isSearching = false;
         }
-        else{
-            if(now_looking_num == target){
+        else
+        {
+            if(now_looking_num == target)
+            {
                 cout << "I found the number." << endl;
                 isSearching = false;
             }
-            else if(now_looking_num < target){
+            else if(now_looking_num < target)
+            {
                 min = ((max + min) /2) + 1; // 今見てるやつ以下は見なくていい
             }
-            else if(now_looking_num > target){
+            else if(now_looking_num > target)
+            {
                 max = ((max + min) /2) - 1; // 今見てるやつ以上は見なくていい
             }
-            if(max < min){
+            if(max < min)
+            {
                 cout << "not found" << endl;
                 isSearching = false;
             }
